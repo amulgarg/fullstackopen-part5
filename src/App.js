548e634c2bda9652ff488cf0c40da388f6c5e0e9
Amul Garg <amulg2012@gmail.com>
2020-11-09 15:47:28 +0530

    part 5 - completed exercise 5.2

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import blogService from './services/blogs';
 import loginService from './services/login';
 
 const App = () => {
+  const loggedInUser = window.localStorage.getItem('user') ? JSON.parse(window.localStorage.getItem('user')) : null;
   const [blogs, setBlogs] = useState([])
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(loggedInUser);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
@@ -26,9 +27,19 @@ const App = () => {
   const loginUser = async (event) => {
     event.preventDefault();
     console.log('username password', username, password);
-    const user = await loginService.login({username, password});
-    console.log('user', user);
-    setUser(user);
+    try{
+      const user = await loginService.login({username, password});
+      window.localStorage.setItem('user', JSON.stringify(user));
+      console.log('user', user);
+      setUser(user);
+    }catch(error){
+      alert(error);
+    }
+  }
+
+  const logout = () => {
+    window.localStorage.removeItem('user');
+    setUser(null);
   }
 
   if(!user){
@@ -51,7 +62,7 @@ const App = () => {
     <div>
       <h2>blogs</h2>
       <br/>
-        <div>{user.name} logged in</div>
+        <div>{user.name} logged in <button onClick={logout}>logout</button></div>
       <br/>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
